c65cc7e9ecbea55326a95fbba841ff1c0b547bde
Tuan Pham Ngoc <github@joomdonation.com>
2018-07-22 11:35:45 +0700

    Hello world

---
+++ admin/code.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @version        1.0
+ * @package        Joomla
+ * @subpackage     Training
+ * @author         Tuan Pham Ngoc
+ * @copyright      Copyright (C) 2016 Ossolution Team
+ * @license        GNU/GPL, see LICENSE.php
+ */
+
+defined('_JEXEC') or die;
+
+echo JText::_('COM_CODE_HELLO_WORLD');
